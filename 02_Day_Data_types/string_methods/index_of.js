e91485d73b962f1691356fd2a57aabb8fea74707
Yash Mittal <112745526+yassmittal@users.noreply.github.com>
2022-10-08 15:27:05 +0530

    remove repetation

--- 02_Day_Data_types/string_methods/index_of.js
+++ 02_Day_Data_types/string_methods/index_of.js
@@ -1,4 +1,4 @@
-// indexOf(): Takes takes a substring and if the substring exists in a string it returns the first position of the substring if does not exist it returns -1
+// indexOf(): Takes  a substring and if the substring exists in a string it returns the first position of the substring if does not exist it returns -1
 
 string.indexOf(substring)
 let string = '30 Days Of JavaScript'
